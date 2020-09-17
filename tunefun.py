67223e0dbf44201b8ca939ade89c4351ea46762d
Will <liquidcitymotors@gmail.com>
2020-09-17 17:47:15 -0500

    nonworking seq example

--- tunefun.py
+++ tunefun.py
@@ -3,6 +3,7 @@ from pippi import fx
 from pippi import dsp
 from pippi.oscs import Osc
 from pippi import wavetables
+from pippi import rhythm
 import os
 import numpy as np
 import lcm
@@ -13,7 +14,6 @@ chord3 = tune.chord('v7', key='a', octave=4)
 chords = [chord1, chord2, chord3]
 
 sr = 48000
-
 length = 1024
 beat = .1
 
@@ -54,39 +54,38 @@ warp2 = warp1.reversed()
 pings &= warp1
 pings &= warp2 * .50
 
-print("Rendering closed hat")
+print("")
 
-main = 'x...x...'
-variations = ['x.x.x...', 'x...x...', 'x.x.x.x.']
+hhmain = 'x...x...'
+hhvariations = ['x.x.x...', 'x...x...', 'x.x.x.x.']
 hat = lcm.random_splice_sound("closed")
 # hat = dsp.read(lcm.get_file())
 velocities = [1, .75, .85, .75]
 
-hatdub = dsp.buffer(length=length*beat)
-
-for note in range(length):
+def makehat(ctx):
+	note = ctx.pos * beats
 	if ((note//16) % 8) == 3:
-		pattern = dsp.choice(variations)
+		pattern = dsp.choice(hhvariations)
 	else:
-		pattern = main
+		pattern = hhmain
 	if pattern[note % len(pattern)] == "x":
 		jitter = (dsp.rand() - .5) * .0001
-		hatdub.dub(hat * velocities[note % len(velocities)], note * beat + jitter)
+		return hat * velocities[note % len(velocities)]
+	else: 
+		return dsp.buffer([0])
 
-print("Rendering snares")
-
-main = '..x.............'
-variations = ['..x.x...', 'x..', 'x....']
+smain = '..x.............'
+svariations = ['..x.x...', 'x..', 'x....']
 snare = lcm.random_splice_sound("snare")
 snare.cut(0, int(snare.dur/beat) * beat)
 revflipflop = 1
-snaredub = dsp.buffer(length=beat * length)
 
-for note in range(length):
+def makesnare(ctx):
+	note = ctx.pos * length
 	if ((note//4) % 4) == 3:
-		pattern = dsp.choice(variations)
+		pattern = dsp.choice(svariations)
 	else:
-		pattern = main
+		pattern = smain
 	if (pattern[note % len(pattern)] == "x") and (dsp.rand() > .5):
 		sound = fx.vspeed2(snare, [dsp.rand() * 2 + .5, 1], 6)
 		if revflipflop > .5: 
@@ -94,27 +93,35 @@ for note in range(length):
 			revflipflop = 0
 		else:
 			revflipflop = 1
-		snaredub.dub(sound, note * beat)
-
-print("Rendering 808")
+		return sound
+	else:
+		return dsp.buffer([0])
 
 bass = lcm.random_splice_sound("MURDA_808")
 bassdub = dsp.buffer(length=length * beat)
-for note in range(length):
+def make808(ctx):
+	note = ctx.pos * length
 	if (note % 16) == 0:
-		bassdub.dub(bass, note * beat)
-
-print("Rendering Perc")
+		return bass
+	else:
+		return dsp.buffer([0])
 
 perc = lcm.random_splice_sound("perc")
 percdub = dsp.buffer(length=length * beat)
-for note in range(length):
+def makeperc(ctx):
+	note = ctx.pos * length
 	if (note % 16) == 8:
 		percdub.dub(perc, note * beat)
 
+dm = rhythm.Seq(beat)
+dm.add('h', 'x', makehat, div=1)
+dm.add('s', 'x', makesnare, div=1)
+dm.add('k', 'x', make808, div=1)
+dm.add('c', 'x', makeperc, div=1)
+out = dm.play(length)
+
 print("Clipping")
 
-out = bassdub & (snaredub * .75) & pings & hatdub & percdub
 out *= .25
 out = fx.upsample(out, 2)
 out.softclip()
@@ -131,8 +138,6 @@ for eightbar in range(length//(16*8)):
 	chop = fx.vspeed2(chop, dsp.choice(pullups), 20, True)
 	end = out.cut(last_bar + bar, max(out.dur - (last_bar + bar), 0))
 	out = first_part + chop + end
-
-
 
 
 lcm.spectrogram(out)
